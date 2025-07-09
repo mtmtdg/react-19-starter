1e04ae7fe5c8eb31e6ea932f7b9346d5416412cc
choug <chougousui@gmail.com>
2025-07-09 22:24:04 +0900

    chore: 初步调整文件夹结构,删除不必要内容

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import './index.scss';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 
-import { App } from './App.tsx';
+import { App } from './app.component';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
